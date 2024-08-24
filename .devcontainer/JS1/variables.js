4d46e359681cb8ecb34e25d21be74a4f19fc9a89
Huskey-Ice <abir59719@gmail.com>
2024-08-24 13:30:34 +0000

    2nd part done

---
+++ .devcontainer/JS1/variables.js
@@ -0,0 +1,10 @@
+const account_Id = 44332211
+var account_Email = "Huskeyice@gmail.com"
+let Account_Password = 12345
+ 
+
+console.table([account_Id,account_Email,Account_Password])
+
+/* use Const and let 
+var used in old browse 
+and it is safe to leave it default withour delearing any thing but don't do that */
