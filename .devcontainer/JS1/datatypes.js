cdad6e300eee0ab7dfa7ab380df682c2689f36de
Huskey-Ice <abir59719@gmail.com>
2024-08-24 18:56:35 +0000

    3rd part done

---
+++ .devcontainer/JS1/datatypes.js
@@ -0,0 +1,14 @@
+let score = 33;
+ console.log(typeof(score));
+
+ console.log(typeof score);
+
+ score ="abc"
+
+ console.log(typeof(score));
+ console.log(typeof score);
+
+ score="33abcd";
+
+ console.log(typeof(score));
+ console.log(typeof score);
