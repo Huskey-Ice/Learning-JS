d03c3fcb469986830c38f7af8aac4724338f269d
Huskey-Ice <abir59719@gmail.com>
2024-08-25 03:19:57 +0000

    5th updation

---
+++ .devcontainer/JS1/05comparison.js
@@ -0,0 +1,10 @@
+/* "==" equity check and comparison check <,>,! are different in JS 
+comparison convert into number if the other side is a number then compare  */
+
+let a =null;
+console.log(a==0);
+console.log(a>=0);
+
+// === strictly check 
+console.log("4"===4);
+console.log("4"==4);
